8e08a68cf8607babaabbe6932c1afc50be179ac1
Tobias Hahn <hahn.tobias66@gmail.com>
2016-07-03 23:13:18 +0200

    Added function to switch teams, dirty modularization

--- js/Header.js
+++ js/Header.js
@@ -1,13 +1,17 @@
 var TeamChooser = React.createClass({
 	
+	componentDidMount: function(){
+		renderTable(this.state.chosenTeam);
+	},
 	
 	getInitialState: function(){
-		return {chosenTeam: {teamName: "Team 1", teamId:0},
+		let initial = {chosenTeam: {teamName: "Team 1", teamId:0},
 				teams: [
 						{teamName: "Team 1", teamId:0},
 						{teamName: "Team 2", teamId:1}
 						]
-		}; 	
+		}; 
+		return initial; 	
 	},	
 
 	handleChange: function(changeEvent){
@@ -19,8 +23,17 @@ var TeamChooser = React.createClass({
 				return;
 			}
 		});
-      		this.state.chosenTeam = chosenTeam;
-		renderHeader(); 
+      	this.state.chosenTeam = chosenTeam;
+		 
+		document.getElementById('teamChooser').dispatchEvent(new CustomEvent(
+			'teamchanged',{
+				detail: chosenTeam,
+				bubbles: true,
+				cancelable: false
+		}));
+		renderHeader();
+		
+		
 	},
 	render: function(){
 		let teams = this.state.teams;
@@ -28,7 +41,7 @@ var TeamChooser = React.createClass({
 		
 		return (
 			<div>
-				<select defaultValue={chosenTeam.teamId} onChange={this.handleChange}>
+				<select id="teamChooser" defaultValue={chosenTeam.teamId} onChange={this.handleChange}>
 				{
 					teams.map(function(team, index){
 					return (
@@ -56,6 +69,62 @@ function renderHeader(){
 		<TeamChooser />,
 		document.getElementsByTagName('header')[0]
 	);
+	
+}
+
+
+
+//Table
+class Table extends React.Component{
+	
+	constructor(){
+		super();
+		
+	}
+	componentDidMount(){
+		document.getElementById('teamChooser').addEventListener('teamchanged',this.createTable, false);
+		console.log(document.getElementById('teamChooser'));
+	}
+
+	createTable(teamChangeEvent){
+		let freelancer = {};
+		console.log(teamChangeEvent);
+		list.forEach(function(curr){
+			if(curr.teamId == teamChangeEvent.detail.teamId){
+				freelancer = curr.freelancer;
+			}
+		})
+		ReactDOM.render(<Table freelancer={freelancer}/>, document.getElementById('freelancerOverview'));
+	}
+
+	render(){
+		let freelancer = this.props.freelancer;
+		return (
+				<div className="table-wrapper" onChange={this.handleChange}>
+					<table className="table table-striped">
+						<tbody>
+						<tr><th>Name</th><th>Projekt</th><th>Start</th><th>Ende</th></tr>
+						{
+							freelancer.map(function(freelancer, index){
+								return (<tr><td>{freelancer.name}</td><td>{freelancer.project}</td><td>{freelancer.startDate}</td><td>{freelancer.startDate}</td></tr>);
+							})
+						}
+						</tbody>
+					</table>
+				</div>
+				);
+	}
+}
+
+
+var renderTable =  function(team){
+	let freelancer = {};
+	list.forEach(function(curr){
+		if(curr.teamId == team.teamId){
+			freelancer = curr.freelancer;
+		}
+	})
+	ReactDOM.render(<Table freelancer={freelancer}/>, document.getElementById('freelancerOverview'));
 }
 
 renderHeader();
---
+++ js/exampleList.json
@@ -0,0 +1,17 @@
+var list = [{
+		teamId:1,
+		freelancer:[
+			{name: "Hans Peter", project: "IT Infrastruture", startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"},
+			{name: "Dr. Hannelore Kohl", project: "Charity Publishing", startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"},
+			{name: "Alice Weißer", project: "Malen lernen",startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"},
+			{name: "Bruce Wayne", project: "Autonomes Fahren", startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"}
+		]
+	},
+	{
+		teamId:0,
+		freelancer:[
+			{name: "Mäddel", project: "SuperCasting", startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"},
+			{name: "Tobi", project: "Kloppibildung", startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"}
+			]
+	}];
+
