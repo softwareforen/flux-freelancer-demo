cfd027ca999773a52c6c249961cfab8b29e2db68
Tobias Hahn <hahn@softwareforen.de>
2016-07-25 15:00:08 +0200

    added function to add freelancer

--- js/exampleList.js
+++ js/exampleList.js
@@ -1,8 +1,8 @@
 var list = 
 		[
-			{id:1, name: "Hans Peter", project: "IT Infrastruture", startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"},
-			{id:2, name: "Dr. Hannelore Kohl", project: "Charity Publishing", startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"},
-			{id:3, name: "Alice Weißer", project: "Malen lernen",startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"},
-			{id:4, name: "Bruce Wayne", project: "Autonomes Fahren", startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"}
+			{name: "Hans Peter", project: "IT Infrastruture", startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"},
+			{name: "Dr. Hannelore Kohl", project: "Charity Publishing", startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"},
+			{name: "Alice Weißer", project: "Malen lernen",startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"},
+			{name: "Bruce Wayne", project: "Autonomes Fahren", startDate: "01.01.1970", endDate:"02.07.1980", role: "Professional"}
 		]
 	
--- js/fluxed/backboneApp.jsx
+++ js/fluxed/backboneApp.jsx
@@ -1,11 +1,26 @@
 
 let FreelancerDispatcher = new Flux.Dispatcher();
+var userInputFreelancer = {
+			name: '',
+			project: '',
+			startDate: '',
+			endDate: ''
+		};
 
 //###############################      STORES    ################################################################
 
 //Items
 //{teamId:0, freelancer:[]}
-let FreelancerItem = Backbone.Model.extend();
+let FreelancerItem = Backbone.Model.extend({
+	attributes: [
+		'name',
+		'project',
+		'role',
+		'startDate',
+		'endDate'
+	]
+	}
+);
 
 var Store = Backbone.Collection.extend({
     model: FreelancerItem,
@@ -14,42 +29,53 @@ var Store = Backbone.Collection.extend({
         this.dispatchToken = FreelancerDispatcher.register(this.dispatchCallback);
         console.log("init store");
         this.add(list);
-	
 	},
     dispatchCallback: function(payload){
-        if(payload.action === "add"){
-			    //FreelancerStore.freelancerList.push(payload.item)
-				console.log(storeInstance.add(payload.item));	
-                console.log(storeInstance);
-			}
-		if(payload.action === "delete"){
-			console.log("delete")
-			let itemToRemove = storeInstance.findWhere(payload.freelancer);
+       if(payload.action === "delete"){
+			let itemToRemove = storeInstance.findWhere(payload.item);
+			console.log(payload.item);
 			storeInstance.remove(itemToRemove);
-			console.log(storeInstance.toJSON());
-			ReactDOM.render(<Table freelancer={storeInstance} />, document.getElementById('content'));
+		}else if(payload.action === "add"){
+			storeInstance.create(payload.item);
+			let input;
+			$('[type = text]').val('');
 		}
+		ReactDOM.render(<Table freelancer={storeInstance} />, document.getElementById('content'));
     },
 
 });
 
-let storeInstance = new Store();
-console.log(storeInstance.toJSON());
 //##############################     Views     ######################################################################
 
-let FreelancerAddComponent = React.createClass({
+let FreelancerActionComponent = React.createClass({
 	handleDelete: function(clickEvent){
 	
 		let freelancer = this.props.freelancer;
-
-		FreelancerDispatcher.dispatch({action:"delete", item:"Hello", freelancer:freelancer});
+		FreelancerDispatcher.dispatch({action:"delete", item:freelancer});
 		
+	},
+
+	handleAdd: function(){
+		let freelancer = this.props.freelancer;
+		console.log(freelancer);
+		FreelancerDispatcher.dispatch({action:"add", item:freelancer});
+		
+	},
+
+	getAction: function(){
+		
+		if(this.props.action === "add"){
+			return this.handleAdd;
+			
+		}else if(this.props.action === "delete"){
+			return this.handleDelete;
+		}
 	},
 	render: function(){
 		
 		return (
 			<div>
-				<a href="#" data={this.props.freelancer} onClick={this.handleDelete}>Delete</a>
+				<a href="#" data={this.props.freelancer} onClick={this.getAction()}>{this.props.action}</a>
             </div>	
 		);
 	}
@@ -61,7 +87,7 @@ let TableRowWrapper = React.createClass({
 	render: function(){
 		let freelancer = this.props.freelancer;
 		return (
-			<tr id={freelancer.id}><td>{freelancer.name}</td><td>{freelancer.project}</td><td>{freelancer.startDate}</td><td>{freelancer.startDate}</td><td><FreelancerAddComponent freelancer={freelancer} onClick={this.handleDelete}/></td></tr>
+			<tr id={freelancer.id}><td>{freelancer.name}</td><td>{freelancer.project}</td><td>{freelancer.role}</td><td>{freelancer.startDate}</td><td>{freelancer.endDate}</td><td><FreelancerActionComponent freelancer={freelancer} action="delete" /></td></tr>
 		);
 	}
 });
@@ -82,20 +108,57 @@ let Table = React.createClass({
 				<div className="table-wrapper">
 					<table className="table table-striped">
 						<tbody>
-						<tr><th>Name</th><th>Projekt</th><th>Start</th><th>Ende</th><th>Actions</th></tr>
+						<tr><th>Name</th><th>Projekt</th><th>Rolle</th><th>Start</th><th>Ende</th><th>Actions</th></tr>
 						{
 							freelancer.map(function(freelancer, index){
 								return <TableRowWrapper key={freelancer.id} freelancer={freelancer} />;
 							})
 						}
+						<TableInput />
 						</tbody>
 					</table>
 				</div>
 				);
 	}
 });
+let TableInput = React.createClass({
+
+	getInitialState: function(){
+		return {freelancer: userInputFreelancer};
+	},
+
+	updateFreelancerDto : function(change){
+		let propName = change.target.getAttribute('data');
+		let propVal = change.target.value;
+		let freelancer = this.state.freelancer;
+		freelancer[propName] = propVal; 
+		this.setState({freelancer: freelancer});
+		
+		 
+	},
+	
+	componentWillReceiveProps: function(){
+		this.replaceState({freelancer: userInputFreelancer});
+	},
+	
+	render: function() {
+		
+		return (
+			<tr>
+				<td><input type="text" data="name" id="freelancerName"  defaultValue={this.state.freelancer.name} onChange={this.updateFreelancerDto} /> </td>
+				<td><input type="text" data="project" id="freelancerProject"  defaultValue={this.state.freelancer.project} onChange={this.updateFreelancerDto} /> </td>
+				<td><input type="text" data="project" id="freelancerProject"  defaultValue={this.state.freelancer.role} onChange={this.updateFreelancerDto} /> </td>
+				<td><input type="text" data="startDate" id="freelancerStartDate"  defaultValue={this.state.freelancer.startDate} onChange={this.updateFreelancerDto} /> </td>
+				<td><input type="text" data="endDate" id="freelancerEndDate"  defaultValue={this.state.freelancer.endDate} onChange={this.updateFreelancerDto} /> </td>
+				<td><FreelancerActionComponent freelancer={this.state.freelancer} action="add" /></td>
+			</tr>
+		);
+	}
+});
 
 
+let storeInstance = new Store();
+console.log(storeInstance.toJSON());
 let table = ReactDOM.render(<Table freelancer={storeInstance} />, document.getElementById('content'));
 
 
